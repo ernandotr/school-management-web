7da72abdc6f8cda80bfd3a9e0f9c952a3b0a2e87
Ernando Rezende <ernando.03@gmail.com>
2025-02-22 23:42:30 +0000

    Added method doPost to receive post requests

--- src/main/java/dev/ernandorezende/servlets/CourseServlet.java
+++ src/main/java/dev/ernandorezende/servlets/CourseServlet.java
@@ -11,19 +11,20 @@ import java.io.IOException;
 
 @WebServlet("/courses")
 public class CourseServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         resp.setContentType("text/html");
-//        PrintWriter out = resp.getWriter();
-//        out.println("<!DOCTYPE html>");
-//        out.println("<html>");
-//        out.println("<head>");
-//        out.println("<title>Servlet CourseServlet</title>");
-//        out.println("</head>");
-//        out.println("<body>");
-//        out.println("<h1>Servlet CourseServlet</h1>");
-//        out.println("</body>");
-//        out.println("</html>");
+        req.getRequestDispatcher("WEB-INF/pages/course.html").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+        String name = req.getParameter("name");
+        String workload = req.getParameter("workLoad");
+        String level = req.getParameter("level");
+        System.out.println("Código: "+code);
         req.getRequestDispatcher("WEB-INF/pages/course.html").forward(req, resp);
     }
 }
