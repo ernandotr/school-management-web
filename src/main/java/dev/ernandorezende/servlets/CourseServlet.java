421029931aa274010c77e38eeb47555b49097258
Ernando Rezende <ernando.03@gmail.com>
2025-02-22 22:18:11 +0000

    Added file course.html and change the servlet load it

--- src/main/java/dev/ernandorezende/servlets/CourseServlet.java
+++ src/main/java/dev/ernandorezende/servlets/CourseServlet.java
@@ -1,28 +1,29 @@
 package dev.ernandorezende.servlets;
 
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/courses")
 public class CourseServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Servlet CourseServlet</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Servlet CourseServlet</h1>");
-        out.println("</body>");
-        out.println("</html>");
+//        PrintWriter out = resp.getWriter();
+//        out.println("<!DOCTYPE html>");
+//        out.println("<html>");
+//        out.println("<head>");
+//        out.println("<title>Servlet CourseServlet</title>");
+//        out.println("</head>");
+//        out.println("<body>");
+//        out.println("<h1>Servlet CourseServlet</h1>");
+//        out.println("</body>");
+//        out.println("</html>");
+        req.getRequestDispatcher("WEB-INF/pages/course.html").forward(req, resp);
     }
 }
